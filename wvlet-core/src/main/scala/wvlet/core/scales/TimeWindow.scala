44733234569e2647e7ca70194d11517aa2c4418f
Taro L. Saito <leo@xerial.org>
2017-06-25 16:06:55 -0700

    Adding time primitives

---
+++ wvlet-core/src/main/scala/wvlet/core/scales/TimeWindow.scala
@@ -0,0 +1,308 @@
+package wvlet.core.scales
+
+import java.time.temporal.ChronoUnit
+import java.time._
+
+
+case class TimeWindow(start:ZonedDateTime, end:ZonedDateTime) {
+
+  private def instantOfStart = start.toInstant
+  private def instantOfEnd = end.toInstant
+
+  def startUnixTime = instantOfStart.getEpochSecond
+  def endUnixTime = instantOfEnd.getEpochSecond
+
+  def startEpochMillis = instantOfStart.toEpochMilli
+  def endEpochMillis = instantOfEnd.toEpochMilli
+
+  override def toString = {
+    val s = TimeStampFormatter.formatTimestamp(startEpochMillis)
+    val e = TimeStampFormatter.formatTimestamp(endEpochMillis)
+    s"[${s}, ${e})"
+  }
+
+  private def splitInto(unit: ChronoUnit): Seq[TimeWindow] = {
+    val b = Seq.newBuilder[TimeWindow]
+    var cursor = start
+    while (cursor.compareTo(end) < 0) {
+      val e = unit match {
+        case ChronoUnit.DAYS | ChronoUnit.HOURS | ChronoUnit.MINUTES =>
+          cursor.plus(1, unit).truncatedTo(unit)
+        case ChronoUnit.WEEKS =>
+          cursor.plus(1, unit).`with`(DayOfWeek.MONDAY)
+        case ChronoUnit.MONTHS =>
+          cursor.plus(1, unit).withDayOfMonth(1)
+        case ChronoUnit.YEARS =>
+          cursor.plus(1, unit).withDayOfYear(1)
+        case other =>
+          throw new IllegalStateException(s"Invalid split unit ${unit} for range ${toString}")
+      }
+      if (e.compareTo(end) <= 0) {
+        b += TimeWindow(cursor, e)
+      }
+      else {
+        b += TimeWindow(cursor, end)
+      }
+      cursor = e
+    }
+    b.result()
+  }
+
+  def splitIntoHours: Seq[TimeWindow] = splitInto(ChronoUnit.HOURS)
+  def splitIntoDays: Seq[TimeWindow] = splitInto(ChronoUnit.DAYS)
+  def splitIntoMonths: Seq[TimeWindow] = splitInto(ChronoUnit.MONTHS)
+  def splitIntoWeeks: Seq[TimeWindow] = splitInto(ChronoUnit.WEEKS)
+}
+
+
+object TimeWindow {
+
+  val systemZone = ZoneId.systemDefault().normalized()
+  val UTC        = ZoneOffset.UTC
+
+  private val unitTable: Map[String, ChronoUnit] = Map(
+    "s" -> ChronoUnit.SECONDS,
+    "m" -> ChronoUnit.MINUTES,
+    "d"-> ChronoUnit.DAYS,
+    "h" -> ChronoUnit.HOURS,
+    "w" -> ChronoUnit.WEEKS,
+    "M" -> ChronoUnit.MONTHS,
+    "y" -> ChronoUnit.YEARS
+  )
+
+  private[scales] def unitOf(s:String) : ChronoUnit = {
+    unitTable.getOrElse(s, throw new IllegalArgumentException(s"Unknown unit type ${s}"))
+  }
+
+
+  def of(zoneId:ZoneId): TimeWindowBuilder = new TimeWindowBuilder(zoneId)
+  def ofUTC: TimeWindowBuilder = new TimeWindowBuilder(UTC)
+  def ofSystem: TimeWindowBuilder = new TimeWindowBuilder(systemZone)
+
+}
+
+
+class TimeWindowBuilder(zone:ZoneId = TimeWindow.systemZone) {
+
+  def durationPattern = "^([+-]|last|next)?([0-9]+)(s|m|d|h|w|M|y)".r("prefix", "num", "unit", "o")
+
+  private def parseDuration(s:String): TimeWindow = {
+    durationPattern.findFirstMatchIn(s) match {
+      case Some(m) =>
+        val length = m.group("num").toInt
+        val unit = TimeWindow.unitOf(m.group("unit"))
+        m.group("prefix") match {
+          case null | "-" | "last" =>
+            last(length).of(unit)
+          case "+" | "next" =>
+            next(length).of(unit)
+          case other =>
+            throw new IllegalArgumentException(s"Unknown duration prefix: ${other}")
+        }
+      case None =>
+        throw new IllegalArgumentException(s"Invalid duration: ${s}")
+    }
+  }
+
+  def parse(str:String): TimeWindow = {
+    val pattern = s"^${durationPattern.regex}(/(.*))?".r("prefix", "num", "unit", "o", "offset")
+    pattern.findFirstMatchIn(str) match {
+      case Some(m) =>
+        val length = m.group("num").toInt
+        val unit = TimeWindow.unitOf(m.group("unit"))
+        val l = m.group("prefix") match {
+          case null | "-" | "last" => last(length)
+          case "+" | "next" => next(length)
+          case other =>
+            throw new IllegalArgumentException(s"Unknown duration prefix: ${other}")
+        }
+        m.group("offset") match {
+          case null => l.of(unit)
+          case "now" => l.untilNow(unit)
+          case other =>
+            l.from(parseDuration(other))
+            throw new IllegalArgumentException(s"Unknown offset: ${other}")
+        }
+      case None =>
+        throw new IllegalArgumentException(s"TimeRange.of(${str})")
+    }
+  }
+
+
+  def now = ZonedDateTime.now(zone)
+  def beginningOfTheHour = now.truncatedTo(ChronoUnit.HOURS)
+  def endOfTheHour = beginningOfTheHour.plusHours(1)
+  def beginningOfTheDay = now.truncatedTo(ChronoUnit.DAYS)
+  def endOfTheDay = beginningOfTheDay.plusDays(1)
+  def beginningOfTheWeek = now.truncatedTo(ChronoUnit.DAYS).`with`(DayOfWeek.MONDAY)
+  def endOfTheWeek = beginningOfTheWeek.plusWeeks(1)
+  def beginningOfTheMonth = now.withDayOfMonth(1).truncatedTo(ChronoUnit.DAYS)
+  def endOfTheMonth = beginningOfTheMonth.plusMonths(1)
+  def beginningOfTheYear = now.withDayOfYear(1).truncatedTo(ChronoUnit.DAYS)
+  def endOfTheYear = beginningOfTheYear.plusYears(1)
+
+
+  trait TimeCursor {
+    def of
+  }
+
+  case class next(x: Long) {
+    def of(unit: ChronoUnit): TimeWindow = {
+      unit match {
+        case ChronoUnit.SECONDS => seconds
+        case ChronoUnit.MINUTES => minutes
+        case ChronoUnit.HOURS => hours
+        case ChronoUnit.DAYS => days
+        case ChronoUnit.WEEKS => weeks
+        case ChronoUnit.MONTHS => months
+        case ChronoUnit.YEARS => years
+        case other =>
+          throw new UnsupportedOperationException(s"next.of(${unit}")
+      }
+    }
+
+    def fromNow(unit: ChronoUnit): TimeWindow = {
+      unit match {
+        case ChronoUnit.SECONDS => seconds
+        case ChronoUnit.MINUTES => minutes
+        case ChronoUnit.HOURS => hoursFromNow
+        case ChronoUnit.DAYS => daysFromNow
+        case ChronoUnit.WEEKS => weeksFromNow
+        case ChronoUnit.MONTHS => monthsFromNow
+        case ChronoUnit.YEARS => yearsFromNow
+        case other =>
+          throw new UnsupportedOperationException(s"next.fromNow(${unit}")
+      }
+    }
+
+    def seconds = {
+      val start = now.truncatedTo(ChronoUnit.SECONDS)
+      TimeWindow(start, start.plusSeconds(x))
+    }
+
+    def minutes = {
+      val start = now.truncatedTo(ChronoUnit.MINUTES)
+      TimeWindow(start, start.plusMinutes(x))
+    }
+
+    def hours = {
+      val currentHour = now.truncatedTo(ChronoUnit.HOURS)
+      TimeWindow(currentHour, currentHour.plusHours(x))
+    }
+
+    def days = {
+      val start = beginningOfTheDay
+      TimeWindow(start, start.plus(x, ChronoUnit.DAYS))
+    }
+    def weeks = {
+      val start = beginningOfTheWeek
+      TimeWindow(start, start.plusWeeks(x))
+    }
+    def months = {
+      val start = beginningOfTheMonth
+      TimeWindow(start, start.plusMonths(x))
+    }
+    def years = {
+      val start = beginningOfTheYear
+      TimeWindow(start, start.plusYears(x))
+    }
+
+    def hoursFromNow = TimeWindow(now, hours.end)
+    def daysFromNow =  TimeWindow(now, days.end)
+    def weeksFromNow = TimeWindow(now, weeks.end)
+    def monthsFromNow = TimeWindow(now, weeks.end)
+    def yearsFromNow = TimeWindow(now, years.end)
+  }
+
+  case class last(x: Long) {
+    def of(unit: ChronoUnit): TimeWindow = {
+      unit match {
+        case ChronoUnit.SECONDS => seconds
+        case ChronoUnit.MINUTES => minutes
+        case ChronoUnit.HOURS => hours
+        case ChronoUnit.DAYS => days
+        case ChronoUnit.WEEKS => weeks
+        case ChronoUnit.MONTHS => months
+        case ChronoUnit.YEARS => years
+        case other =>
+          throw new UnsupportedOperationException(s"last.of(${unit}")
+      }
+    }
+
+    def untilNow(unit: ChronoUnit): TimeWindow = {
+      unit match {
+        case ChronoUnit.SECONDS => seconds
+        case ChronoUnit.MINUTES => minutes
+        case ChronoUnit.HOURS => hoursUntilNow
+        case ChronoUnit.DAYS => daysUntilNow
+        case ChronoUnit.WEEKS => weeksUntilNow
+        case ChronoUnit.MONTHS => monthsUntilNow
+        case ChronoUnit.YEARS => yearsUntilNow
+        case other =>
+          throw new UnsupportedOperationException(s"last.fromNow(${unit}")
+      }
+    }
+
+    def seconds = {
+      val end = now.truncatedTo(ChronoUnit.SECONDS)
+      val start = end.minusSeconds(x)
+      TimeWindow(start, end)
+    }
+
+    def minutes = {
+      val end = now.truncatedTo(ChronoUnit.MINUTES)
+      val start = end.minusMinutes(x)
+      TimeWindow(start, end)
+    }
+
+    def hours = {
+      val currentHour = now.truncatedTo(ChronoUnit.HOURS)
+      val start = currentHour.minusHours(x)
+      TimeWindow(start, currentHour)
+    }
+
+    def hoursUntilNow = {
+      TimeWindow(hours.start, now)
+    }
+
+    def days = {
+      val now = beginningOfTheDay
+      val start = now.minus(x, ChronoUnit.DAYS)
+      TimeWindow(start, now)
+    }
+
+    def daysUntilNow = {
+      val d = days
+      TimeWindow(d.start, now)
+    }
+
+    def weeks = {
+      val end = beginningOfTheWeek
+      val start = end.minusWeeks(x)
+      TimeWindow(start, end)
+    }
+
+    def weeksUntilNow = {
+      TimeWindow(weeks.start, now)
+    }
+
+    def months = {
+      val now = beginningOfTheMonth
+      val start = now.minusMonths(x)
+      TimeWindow(start, now)
+    }
+
+    def monthsUntilNow = {
+      TimeWindow(months.start, now)
+    }
+
+    def years = {
+      val end = beginningOfTheYear
+      val start = end.minusYears(x)
+      TimeWindow(start, end)
+    }
+
+    def yearsUntilNow = TimeWindow(years.start, now)
+  }
+
+}
---
+++ wvlet-core/src/test/scala/wvlet/core/scales/TimeWindowTest.scala
@@ -0,0 +1,91 @@
+package wvlet.core.scales
+
+import wvlet.test.WvletSpec
+
+/**
+  *
+  */
+class TimeWindowTest extends WvletSpec {
+
+  val t = TimeWindow.ofSystem
+  info(s"now: ${t.now}")
+
+  def parse(s:String): TimeWindow = {
+    val w = t.parse(s)
+    info(s"str:${s}, window:${w}")
+    w
+  }
+
+  "TimeWindow" should {
+
+    "parse string repl" in {
+      // duration/offset
+
+      // DURATION := (+ | -)?(INTEGER)(UNIT)
+      // UNIT     := s | m | h | d | w | M | y
+      //
+      // OFFSET   := DURATION | DATE_TIME
+      // RANGE    := (DURATION) (/ (OFFSET))?
+      // DATE_TIME := yyyy-MM-dd( HH:mm:ss(.ZZZ| ' ' z)?)?
+      //
+
+      // The default offset is 0(UNIT) (the beginning of the given time unit)
+
+      // 7 days ago until at the beginning of today.
+      // 0d := the beginning of the day
+      // [-7d, 0d)
+      parse("7d")
+
+      // [-7d, 0d)
+      // |-------------|
+      // -7d -- ... -- 0d ---- now  ------
+      parse("-7d")
+
+      // [0d, +7d)
+      //      |------------------------------|
+      // ---  0d --- now --- 1d ---  ... --- 7d
+      parse("+7d")
+
+      // Since 7 days ago + time fragment from [-7d, now)
+      //  |-------------------|
+      // -7d - ... - 0d ---- now  ------
+      parse("7d/now")
+
+      // [now, +7d)
+      //         |---------------------|
+      // 0d --- now --- 1d ---  ... --- 7d
+      parse("+7d/now")
+
+      // [-1h, 0h)
+      parse("1h")
+      // [-1h, now)
+      parse("1h/now")
+
+
+      // -12hU:now  (last 12 hours + fraction until now. U is effective only for negative duration)
+      // -12hF:now  (-12 hours from now. No trunctation)
+      // +12hF:now  (+12 hours from now)
+
+      parse("-12h/now")
+      parse("-12h")
+      parse("-12h/now")
+      parse("+12h/now")
+
+      // Absolute offset
+      // 3d:2017-04-07 [2017-04-04,2017-04-07)
+      parse("3d/2017-04-07")
+
+      // The offset can be specified using a duration
+      // -1M:-1M  [2017-04-01, 2017-05-01) if today is 2017-05-20
+      parse("1M/0M")
+      // -1M:-1M  [2017-03-01, 2017-04-01) if today is 2017-05-20
+      parse("1M/1M")
+
+      // -1h/2017-01-23 01:00:00 -> [2017-01-23 00:00:00,2017-01-23 01:00:00]
+      // -1h/2017-01-23 01:23:45 -> [2017-01-23 00:00:00,2017-01-23 01:23:45]
+      // 60m/2017-01-23 01:23:45 -> [2017-01-23 00:23:45,2017-01-23 01:23:45]
+
+    }
+  }
+
+}
