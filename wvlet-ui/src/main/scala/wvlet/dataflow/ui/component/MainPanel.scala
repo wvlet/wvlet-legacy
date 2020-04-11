5d9df7a972e79d80f55a927e8876dae37084c3fe
Taro L. Saito <leo@xerial.org>
2020-04-10 17:53:14 -0700

    Add navbar

--- wvlet-ui/src/main/scala/wvlet/dataflow/ui/component/MainPanel.scala
+++ wvlet-ui/src/main/scala/wvlet/dataflow/ui/component/MainPanel.scala
@@ -19,8 +19,100 @@ import wvlet.airframe.http.rx.html.all._
 /**
   *
   */
-class MainPanel extends RxElement {
+class MainPanel(menuHeader: MenuHeader, header: Header) extends RxElement {
   override def render: RxElement = {
-    div("Hello wvlet!")
+    div(
+      cls -> "container-fluid p-0",
+      div(
+        cls -> "d-flex flex-row",
+        div(cls -> "p-0", menuHeader),
+        div(cls -> "p-0 container-fluid",
+            header,
+            div(
+              "Hello wvlet",
+              hr()
+            ))
+      )
+    )
+  }
+}
+
+class Header extends RxElement {
+  override def render: RxElement = {
+    div(
+      cls -> "container-fluid",
+      div(
+        cls -> "d-flex justify-content-end",
+        a(
+          cls   -> "nav-link",
+          style -> "text-decoration: none; color: #97909F;",
+          href  -> "#login",
+          i(cls -> s"fas fa-cog", style -> "width: 20px;"),
+          " Settings"
+        ),
+        a(
+          cls   -> "nav-link",
+          style -> "text-decoration: none; color: #97909F;",
+          href  -> "#login",
+          i(cls -> s"fas fa-user-circle", style -> "width: 20px;"),
+          " Login"
+        )
+      )
+    )
+  }
+}
+
+class MenuHeader extends RxElement {
+
+  def navItem(name: String, iconCls: String) = {
+    ul(
+      cls   -> "p-0 m-0",
+      style -> "list-style-type: none;",
+      li(
+        cls   -> "nav-item",
+        style -> "white-space: nowrap;",
+        a(
+          cls   -> "nav-link pl-2 pr-4",
+          style -> "text-decoration: none; color: #97909F; list-style-type: none; ",
+          href  -> "#settings",
+          i(cls -> iconCls, style -> "width: 20px;"),
+          s" ${name}"
+        )
+      )
+    )
+  }
+
+  override def render: RxElement = {
+    nav(
+      cls -> "collapse navbar-collapse d-block",
+      div(
+        cls -> "sidebar-content",
+        div(
+          cls -> "d-flex justify-content-center",
+          a(
+            cls   -> "navbar-brand",
+            style -> "text-decoration: inherit; color: inherit; ",
+            href  -> "#",
+            img(
+              cls    -> "d-inline-block",
+              src    -> "img/logo-40px.png",
+              alt    -> "wvlet",
+              width  -> 40,
+              height -> 40
+            )
+          )
+        ),
+        div(cls -> "sidebar-menu",
+            navItem("Databases", "fas fa-database"),
+            navItem("Queries", "fas fa-binoculars"),
+            navItem("Notebooks", "fas fa-book-open")),
+//
+//        div(
+//          cls   -> "sidebar-footer py-2",
+//          style -> "position: absolute; bottom: 0px; display: flex;",
+//          navItem("Settings", "fas fa-cog")
+//        )
+      )
+    )
   }
 }
