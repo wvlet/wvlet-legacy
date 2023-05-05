fcf1589910d7eebbb11bb248b58cf794ee115b8a
Taro L. Saito <leo@xerial.org>
2023-05-05 11:05:28 -0700

    Add an UI project (#274)
    * Add UI project structure
    
    * Fix vite setup
    
    * Add bootstrap CSS

---
+++ wvlet-ui/vite.config.js
@@ -0,0 +1,56 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { spawnSync } from "child_process";
+import { defineConfig } from "vite";
+
+function isDev() {
+    return process.env.NODE_ENV !== "production";
+}
+
+function printSbtTask(task) {
+    const args = ["--error", `print ${task}`];
+    const options = {
+        cwd: "..",
+        stdio: [
+            "pipe", // StdIn.
+            "pipe", // StdOut.
+            "inherit", // StdErr.
+        ],
+    };
+    const result = process.platform === 'win32'
+        ? spawnSync("sbt.bat", args.map(x => `"${x}"`), {shell: true, ...options})
+        : spawnSync("sbt", args, options);
+
+    if (result.error)
+        throw result.error;
+    if (result.status !== 0)
+        throw new Error(`sbt process failed with exit code ${result.status}`);
+    return result.stdout.toString('utf8').trim();
+}
+
+const replacementForPublic = isDev()
+    ? printSbtTask("ui/publicDev")
+    : printSbtTask("ui/publicProd");
+
+export default defineConfig({
+    resolve: {
+        alias: [
+            {
+                find: "@public",
+                replacement: replacementForPublic,
+            },
+        ],
+    },
+});
